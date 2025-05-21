d802cee5b926843bcd26d24a9cf8f3d85a9dc03f
Hammmzl <muhammadilham15729989@gmail.com>
2025-05-21 11:26:14 +0700

    first commit, upload aplikasi manajemen renew energy

---
+++ app/main/forms.py
@@ -0,0 +1,37 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, DecimalField, DateField, IntegerField, FieldList, FormField, SubmitField
+from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, NumberRange
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+
+
+class WasteOilPurchaseForm(FlaskForm):
+    nama_pengepul = StringField('Nama Pengepul', validators=[DataRequired()])
+    tanggal_pembelian = DateField('Tanggal Pembelian', validators=[DataRequired()])
+    jumlah = DecimalField('Jumlah (liter)', validators=[DataRequired(), NumberRange(min=0)])
+    harga_per_liter = DecimalField('Harga per Liter', validators=[DataRequired(), NumberRange(min=0)])
+    submit = SubmitField('Simpan')
+
+
+
+class SuratJalanDetailForm(FlaskForm):
+    nama_barang = StringField('Nama Barang', validators=[DataRequired()])
+    jumlah = IntegerField('Jumlah', validators=[DataRequired()])
+    keterangan = StringField('Keterangan')
+    submit = SubmitField('Simpan')
+
+class SuratJalanForm(FlaskForm):
+    nomor = StringField('No. Surat Jalan', validators=[DataRequired()])
+    tanggal = DateField('Tanggal', validators=[DataRequired()])
+    no_kendaraan = StringField('No. Kendaraan', validators=[DataRequired()])
+    tujuan = StringField('Tujuan', validators=[DataRequired()])
+    owner = StringField('Owner', validators=[DataRequired()])
+    supir = StringField('Supir', validators=[DataRequired()])
+    penerima = StringField('Penerima', validators=[DataRequired()])
+    submit = SubmitField('Simpan')
+
