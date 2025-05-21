92f30f24fb39f8edb8c8758a1f389e99f87e3d20
Hammmzl <muhammadilham15729989@gmail.com>
2025-05-21 11:42:02 +0700

    Remove deleted files after stash pop and cleanup

--- app/auth/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-auth_bp = Blueprint('auth', __name__, template_folder='templates/volt_dashboard')
-
-from . import routes  # nanti import routes auth biar route terdaftar
--- app/config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    SECRET_KEY = 'supersecretkey'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:19desember2000@localhost/minyak_jelantah_db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
