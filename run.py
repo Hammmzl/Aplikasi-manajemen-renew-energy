d17857fc55bb0a8a988594a0b71e306368519d6f
Hammmzl <muhammadilham15729989@gmail.com>
2025-05-23 14:24:01 +0700

    commit terakhir sebelum deploy

--- run.py
+++ run.py
@@ -1,9 +1,7 @@
-from app import create_app, db
-from flask_migrate import Migrate
-from app.models import User
+from app import create_app
 
 app = create_app()
-migrate = Migrate(app, db)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
